09ad1fa83b19c7793eb3031d30cde12003007731
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-07-27 13:44:33 +0300

    balance component change

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import Header from './components/Header'
-import './App.css'
+import React from "react";
+import Header from "./components/Header";
+import "./App.css";
+import Balance from "./components/Balance";
 
 function App() {
- 
   return (
     <>
       <div>
-        <Header/> 
+        <Header />
+        <Balance />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
