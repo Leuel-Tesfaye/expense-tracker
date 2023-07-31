f9231ddacaada92744581aa2e8445cbe3c6d6fef
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-07-31 09:13:02 +0300

    push commit

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 function Transaction({ transaction }) {
+  const sign = transaction.amount < 0 ? "-" : "+";
   // here we can use keyword props and use it props.transaction.text but not we destructure it
   return (
     <>
-      <li className="minus">
-        {transaction.text} <span>-$400</span>
+      <li className={transaction.amount < 0 ? "minus" : "plus"}>
+        {transaction.text}{" "}
+        <span>
+          {sign}${Math.abs(transaction.amount)}
+        </span>
         <button className="delete-btn">X</button>
       </li>
     </>
