dc8f1f7089c926910cc03061b02e31f6650727ac
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-07-31 10:35:30 +0300

    adding a new transaction change commit

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 function AddTransaction() {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
 
+  const { addTransaction } = useContext(GlobalContext);
+
   return (
     <>
       <h3>Add new transaction </h3>
-      <form>
+      <form onSubmit={}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
